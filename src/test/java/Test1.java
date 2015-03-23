88a337b3ff2c4da0ecc580513c0fa49ae703ba92
beaven1985 <Spoofing01>
2015-03-23 12:11:11 +0000

    setting up junit

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.function.Function;
+
 /**
  * Created by stefos on 19/03/2015.
  */
@@ -7,6 +9,11 @@ public class Test1 {
 
     @Test
     public void lamdbaTest(){
+
+        // Lambda Runnable
+        Runnable r2 = () -> System.out.println("Hello world two!");
+
+        r2.run();
 
     }
 }
