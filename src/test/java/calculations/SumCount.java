82d488b1c86701fb90fec437ddd9e3dcef356fca
beaven1985 <stephenfoster1985@gmail.com>
2015-03-31 23:03:25 +0100

    Sorting

--- src/test/java/calculations/SumCount.java
+++ src/test/java/calculations/SumCount.java
@@ -12,7 +12,6 @@ public class SumCount {
     @Test
     public void test(){
 
-
         List<Integer> numbers = Arrays.asList(1, 5, 2, 5, 7, 9, 3);
 
         int sum = numbers.stream().mapToInt(myInt -> myInt).sum();
@@ -24,6 +23,14 @@ public class SumCount {
         List<Long> longs = Arrays.asList(1L, 5L, 2L, 5L, 7L, 9L, 3L);
 
         long sum1 = longs.stream().mapToLong(myLong -> myLong).sum();
+
+        LongSummaryStatistics longSummaryStatistics = longs.stream().mapToLong(myLong -> myLong).summaryStatistics();
+
+        longSummaryStatistics.getAverage();
+        longSummaryStatistics.getCount();
+        longSummaryStatistics.getMax();
+        longSummaryStatistics.getMin();
+        longSummaryStatistics.getSum();
 
         System.out.print(sum1);
     }
---
+++ src/test/java/sorting/SortingUsingLambdas.java
@@ -0,0 +1,15 @@
+package sorting;
+
+import org.junit.Test;
+
+/**
+ * Created by St on 31/03/2015.
+ */
+public class SortingUsingLambdas {
+
+    @Test
+    public void test(){
+
+
+    }
+}
